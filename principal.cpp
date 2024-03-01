c5bffe721d365bd8bee17ab6040561dedf7ef107
Laurah134 <laurahmy134@gmail.com>
2024-03-01 01:51:25 -0500

    variables de entrada

--- principal.cpp
+++ principal.cpp
@@ -1,2 +1,16 @@
 #include<iostream>
 #include<cmath>
+
+int menu(int o)
+{
+    std::cout<<"::::::MENU::::::"<<std::endl;
+    std::cout<<"\t1. Suma\n\t 2. Resta\n\t 3. División\n\t 4. Multiplicación\n\t 5. Modulo\n\t"<<std::endl;
+    std::cin>>o;
+    return o;
+}
+double rnum(double n)
+{
+    std::cout<<"Ingrese un número"<<std::endl;
+    std::cin>>n;
+    return n;
+}
